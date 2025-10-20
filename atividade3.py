3ad0d9d7acac7bfbd42681bc1781617e1718ed8d
Natalia <nataliabeapacheco@gmail.com>
2025-10-20 20:33:26 -0300

    first commit

---
+++ atividade3.py
@@ -0,0 +1,9 @@
+nome= input('digite seu nome') 
+cupom= input(' digite seu cupom')
+desconto= 0.1 
+
+print(f'olá, {nome} em janeiro você realizou uma compra nop valor de R$500,00 e ganhou um desconto de 10% em sua próxima compra, uso nosso cupom {nome}É10')
+
+valor_da_compra=500
+valor_da_compra* 0.1
+print(f"sua compra era de R${valor_da_compra}. Com descontos sua compra é de R$ {valor_da_compra - desconto}")
